41b1101072504e967c0e551f396f686d4c799f96
Jiayang <wujy@clounix.com>
2023-03-16 23:46:03 +0800

    add shell demo

--- test5.sh
+++ test5.sh
@@ -29,9 +29,30 @@ echo ${string[0]}
 string="my name is Jacky"
 echo ${string:1:5}
 
-#echo `expr index "$string" y`
-string="runoob is a great site"
-echo `expr index "$string" io`  # 输出 4
+echo `expr index "$string" y`
+# string="runoob is a great site"
+# echo `expr index "$string" io`  # 输出 4
+
+echo `expr index "$string" m`
 
 
+arr1=(1 2 3 4 5 6)
+
+echo ${arr1[1]}
+arr1[6]=7
+echo ${arr1[6]}
+
+echo ${arr1[@]}
+
+lengthn=${#arr1[0]}
+echo $lengthn
+
+arr2=("abc" "aaaaa" "afafafafaf")
+echo ${#arr2[0]}
+echo ${#arr2[1]}
+
+:<<EOF
+kdjgkjgkjkakg
+dkgjajgk
+EOF
 
