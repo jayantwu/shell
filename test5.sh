5bb008f02caf427be69a6d956672e4b322a6b154
“wujiayang” <1064216585@qq.com>
2023-03-16 20:55:53 +0800

    add demo

---
+++ test5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "hello world"
+
+your_name="sssss"
+echo $your_name
+
+
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
+
+
+greeing="hello "$your_name" !"
+greeting2="hello $your_name !"
+echo $greeing
+echo $greeting2
+
+echo $greeing $greeting2
+
+
+string='abcd'
+
+echo $string
+echo ${#string}
+echo ${#string[0]}
+echo ${string[0]}
+
+string="my name is Jacky"
+echo ${string:1:5}
+
+#echo `expr index "$string" y`
+string="runoob is a great site"
+echo `expr index "$string" io`  # 输出 4
+
+
+
